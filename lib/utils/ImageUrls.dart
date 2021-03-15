c516b1eca43489e6deb2842b0760f7073b755fd5
unknown <thangaduraiselvaraj04@gmail.com>
2021-03-15 20:24:23 +0530

    fevi icon and bug fixes

--- lib/utils/ImageUrls.dart
+++ lib/utils/ImageUrls.dart
@@ -1,27 +1,27 @@
 class ImageUrls {
 
   // Social images
-  static const INSTA_PATH = "images/instagram.png";
-  static const GIT_HUB_PATH = "images/github.png";
-  static const LINKED_IN_PATH = "images/linkedin.png";
-  static const TWITTER_PATH = "images/twitter.png";
-  static const EMAIL_PATH = "images/email.png";
+  static const INSTA_PATH = "assets/images/instagram.png";
+  static const GIT_HUB_PATH = "assets/images/github.png";
+  static const LINKED_IN_PATH = "assets/images/linkedin.png";
+  static const TWITTER_PATH = "assets/images/twitter.png";
+  static const EMAIL_PATH = "assets/images/email.png";
 
   // Logo images
-  static const ANDROID_LOGO_PATH = "images/android_logo.png";
-  static const FLUTTER_LOGO_PATH = "images/flutter_logo.png";
+  static const ANDROID_LOGO_PATH = "assets/images/android_logo.png";
+  static const FLUTTER_LOGO_PATH = "assets/images/flutter_logo.png";
 
   // Alvin images
-  static const ALVIN_LOGO = "images/badge.png";
+  static const ALVIN_LOGO = "assets/images/badge.png";
 
   // code images
-  static const CODE_WITH_BRACKETS = "images/code_with_brackets.png";
-  static const CODE_WITH_TEXT = "images/code_with_text.png";
+  static const CODE_WITH_BRACKETS = "assets/images/code_with_brackets.png";
+  static const CODE_WITH_TEXT = "assets/images/code_with_text.png";
 
   // Animation bg
-  static const CIRCULAR_BG_ANIMATION = "images/circular_bg.json";
-  static const CODING_IN_OFFICE_ANIMATION = "images/coding_in_office.json";
-  static const CODING_BG_ANIMATION = "images/coding_bg.json";
+  static const CIRCULAR_BG_ANIMATION = "assets/images/circular_bg.json";
+  static const CODING_IN_OFFICE_ANIMATION = "assets/images/coding_in_office.json";
+  static const CODING_BG_ANIMATION = "assets/images/coding_bg.json";
 
 
 }
--- lib/utils/SocialNetworkUrls.dart
+++ lib/utils/SocialNetworkUrls.dart
@@ -1,9 +1,7 @@
 class SocialNetworkUrls {
-  static const LINKED_IN =
-      "https://www.linkedin.com/in/thangadurai-selvaraj-8546bb165/";
+  static const LINKED_IN = "https://www.linkedin.com/in/thangadurai-selvaraj-8546bb165/";
   static const GIT_HUB = "https://github.com/THANGADURAISELVARAJ";
   static const INSTAGRAM = "https://www.instagram.com/a_l_v_i_n_gold/";
   static const TWITTER = "https://twitter.com/allunivingold";
-  static const RESUME_URL =
-      "https://media-exp1.licdn.com/dms/document/C562DAQEMfBELe1qiGg/profile-treasury-document-pdf-analyzed/0/1614582961057?e=1614672000&v=beta&t=di5nV5lWFHtS47rdx8g65Z5Dn0p19X5OEoHoeCk3BTg";
+  static const RESUME_URL = "https://github.com/THANGADURAISELVARAJ/Resume/raw/main/Full%20Resume.pdf";
 }
